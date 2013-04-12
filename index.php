bbdc7d491388c0ae04d3c76f2137c235a0738a83
Renan Veroneze <renan@agenciagenia.com.br>
2013-04-12 15:53:53 -0300

    First Commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <title>Título</title>
+
+    <!-- Css -->
+    <link rel="stylesheet" href="css/main.css">
+</head>
+<body>
+    
+
+    <!-- Js -->
+    <script src="js/jquery.js"></script>
+    <script src="js/app.js"></script>
+
+</body>
+</html>
