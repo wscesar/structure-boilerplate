9a03d7df116a1994393ce9377e3f7982e6a2bfd2
renanveroneze <veroneze.renan@gmail.com>
2013-04-22 15:16:18 -0300

    Adding SMTP simple class and send-contact.php

---
+++ inc/send-contact.php
@@ -0,0 +1,29 @@
+<?php 
+    
+    include 'smtp.php';
+
+    /*  ==========================================================================
+        Send Contact Action
+        ==========================================================================  */
+
+    // Config
+
+    $smtp->host = ''; // smtp.myserver.com
+    $smtp->user = ''; // myuser@myserver.com
+    $smtp->pass = ''; // mypop3password
+
+
+    // Message
+
+    $msg = '<table>
+                <tr>
+                    <td>oiçá</td>
+                </tr>
+            </table>';
+
+
+    // Send
+
+    $smtp->send('to@email.com', 'Subject', $msg);
+
+?>
---
+++ inc/smtp.php
@@ -0,0 +1,51 @@
+<?php
+
+    /* --- SMTP Simple class created by Renan Veroneze © 2013 --- */    
+
+    /*  ==========================================================================
+        SMTP => Send mail
+        ==========================================================================  */
+    
+    class SMTP {
+
+        var $host;
+        var $user;
+        var $pass;
+
+        var $port = 587;
+
+        public function put($value) {
+            return fputs($this->smtp, $value."\n");
+        }
+
+        public function send($to, $subject, $message, $from = false) {
+
+            if(!$from) $from = $this->user;
+
+            $headers  = "Content-type: text/html\n";
+            $headers .= "To: <".$to.">\n";
+            $headers .= "From: <".$from.">\n";
+            $headers .= "Subject: ".$subject."\n\n";
+            
+            $headers .= "<html><body>".$message."</body></html>\n.\n";
+
+            $this->smtp = fsockopen($this->host, $this->port);
+
+            $this->put("EHLO localhost");
+            $this->put("AUTH LOGIN");
+            $this->put(base64_encode($this->user));
+            $this->put(base64_encode($this->pass));
+            $this->put("MAIL FROM: <".$from.">");
+            $this->put("RCPT TO: <".$to.">");
+            $this->put("DATA");
+            $this->put($headers);
+            $this->put("QUIT");
+
+            fclose($this->smtp);
+        }
+
+    }
+
+    $smtp = new SMTP();
+
+?>
