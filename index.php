2819d27f7958f4d3fbebaca0a53065a702404beb
renanveroneze <veroneze.renan@gmail.com>
2013-04-25 20:13:23 -0300

    Removing attribute  to analytics code

--- index.php
+++ index.php
@@ -41,7 +41,6 @@
 
         (function() {
             var ga = document.createElement('script'); 
-                ga.type = 'text/javascript'; 
                 ga.async = true;
                 ga.src = (location.protocol == 'https:' ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
             document.getElementsByTagName('script')[0].parentNode.insertBefore(ga);
